49bc66f12765a3bfbe6f3b47741f4c2f6ae01998
Edan's Win11 <961150665@qq.com>
2024-07-23 17:49:47 +0800

    feat: 增加获取所有能领取的优惠券列表接口

--- src/api/controller/coupons.js
+++ src/api/controller/coupons.js
@@ -3,7 +3,8 @@ const moment = require('moment');
 
 module.exports = class extends Base {
     /**
-     * 获取时令菜单
+     * 获取优惠券总列表
+     * （无任何限制条件）
      * @returns {Promise.<*>}
      */
     async listAction() {
@@ -14,4 +15,33 @@ module.exports = class extends Base {
 		// console.log('=-=-=-> listAction -> list: ', list)
         return this.success(list);
     }
+
+    /**
+     * 获取该用户的优惠券
+     * 即，表示出不可领取的
+     */
+    async getAllCouponsAction() {
+		// console.log('=-=-=-> func -> getAllCouponsAction')
+        const userId = this.getLoginUserId();
+
+        const couponsModel = this.model('coupons');
+		const couponsList = await couponsModel.select();
+        // console.log('=-=-=-> 所有优惠券 couponsList:\n', JSON.stringify(couponsList, null, 4));
+
+        const userCouponsModel = this.model('user_coupons');
+		const userCouponsList = await userCouponsModel.where({ user_id: userId }).select();
+        // console.log('=-=-=-> 该用户领过的优惠券 couponsList:\n', JSON.stringify(userCouponsList, null, 4));
+
+        // 遍历 userCouponsList 列表，把 couponsList 列表中不能领取的标识出来
+        couponsList.map(c => {
+            const got = userCouponsList.find(uc => uc.coupon_id == c.id)
+            // console.log('=-=-=-> got:\n', JSON.stringify(got, null, 4))
+            // 如果领取过，则添加 canGet 字段
+            c.canGet = got == undefined
+            return c
+        })
+
+        return this.success(couponsList);
+    }
+
 };
--- src/api/controller/user_coupons.js
+++ src/api/controller/user_coupons.js
@@ -18,4 +18,41 @@ module.exports = class extends Base {
 		// console.log('=-=-=-> listAction -> list: ', list)
         return this.success(list);
     }
+
+    /**
+     * 领取优惠券到用户账户
+     */
+    async getCouponAction() {
+		console.log('=-=-=-> func -> getCouponAction')
+
+		const userId = this.getLoginUserId();
+        console.log('=-=-=-> getCouponAction -> userId: ', userId)
+
+        const coupon_id = this.post("coupon_id")
+        console.log('=-=-=-> getCouponAction -> coupon_id: ', coupon_id)
+
+        const userCouponInfo = {
+            user_id: userId,
+            coupon_id: coupon_id
+        }
+        console.log('=-=-=-> getCouponAction -> userCouponInfo:\n', JSON.stringify(userCouponInfo, null, 4))
+
+        const model = this.model('user_coupons');
+		const item = await model.where(userCouponInfo).count()
+        console.log('=-=-=-> getCouponAction -> item:\n', JSON.stringify(item, null, 4))
+
+        // 多一层校验，数据库是否已经存在此关联
+        if (item != 0) {
+            return this.fail(400, "您已经领取过该优惠券")
+        }
+
+        // 将该关联关系存入数据库
+        const userCouponId = await model.add(userCouponInfo);
+        console.log('=-=-=-> getCouponAction -> userCouponId: ', userCouponId)
+
+        return this.success({
+            userCouponInfo: userCouponInfo
+        })
+    }
+
 };
